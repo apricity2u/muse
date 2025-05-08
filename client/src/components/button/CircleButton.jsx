37e0063a23aed2af474869e912aaa5f42d964077
ysc13245 <ysc13245@gmail.com>
2025-05-09 04:16:16 +0900

    feat: CircleButton 컴포넌트 스타일

--- client/src/components/button/CircleButton.jsx
+++ client/src/components/button/CircleButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './CircleButton.module.css';
 
-export default function CircleButton({ image, clickHandler, icon }) {
+export default function CircleButton({ clickHandler, content = '좋아요' }) {
   const icons = { '리뷰 작성': '/edit.svg', '링크 복사': '/link.svg', 좋아요: '/heart.svg' };
 
   return (
     <div className={styles.wrapper}>
       <button className={styles.circle} onClick={clickHandler}>
-        <img src={icons[icon]} alt={icon} />
+        <img src={icons[content]} alt={content} />
+        <div className={styles.tooltip}>{content}</div>
       </button>
-      <div className={styles.tooltip}>{icon}</div>
     </div>
   );
 }
