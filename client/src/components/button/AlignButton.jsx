a6e99fe775cbc79bd5bd7c176f1b1f24ff7e3e44
ysc13245 <ysc13245@gmail.com>
2025-05-09 03:48:04 +0900

    feat: circlebutton

--- client/src/components/button/AlignButton.jsx
+++ client/src/components/button/AlignButton.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import styles from './AlignButton.module.css';
 
 /**
- * @param {*} clickHandler1 최신순 눌렀을 때 동작할 이벤트
- * @param {*} clickHandler2 좋아요순 눌렀을 때 동작할 이벤트
- * @param {*} selected 선택된 버튼을 결정하는 state
+ * @param clickHandler1 최신순 눌렀을 때 동작할 이벤트
+ * @param clickHandler2 좋아요순 눌렀을 때 동작할 이벤트
+ * @param selected 선택된 버튼을 결정하는 state
  */
-export default function AlignButton({ clickHandler1, clickHandler2, selected="최신순" }) {
+export default function AlignButton({ clickHandler1, clickHandler2, selected = '최신순' }) {
   return (
     <>
-      <button onClick={clickHandler1} className={selected === "최신순" ? styles.selected : styles.button}>
+      <button
+        onClick={clickHandler1}
+        className={selected === '최신순' ? styles.selected : styles.button}
+      >
         최신순
       </button>
 
-      <button onClick={clickHandler2} className={selected === "좋아요순" ? styles.selected : styles.button}>
+      <button
+        onClick={clickHandler2}
+        className={selected === '좋아요순' ? styles.selected : styles.button}
+      >
         좋아요순
       </button>
     </>
--- client/src/components/button/CircleButton.jsx
+++ client/src/components/button/CircleButton.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from './CircleButton.module.css';
 
-export default function CircleButton() {
+export default function CircleButton({ image, clickHandler, icon }) {
+  const icons = { '리뷰 작성': '/edit.svg', '링크 복사': '/link.svg', 좋아요: '/heart.svg' };
+
   return (
-    <div>CircleButton</div>
-  )
+    <div className={styles.wrapper}>
+      <button className={styles.circle} onClick={clickHandler}>
+        <img src={icons[icon]} alt={icon} />
+      </button>
+      <div className={styles.tooltip}>{icon}</div>
+    </div>
+  );
 }
