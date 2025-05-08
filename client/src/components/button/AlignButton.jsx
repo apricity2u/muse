85fc93cc00143e0f2320c361014494e3270b7c7e
ysc13245 <ysc13245@gmail.com>
2025-05-08 21:34:38 +0900

    feat: AlignButton 컴포넌트

--- client/src/components/button/AlignButton.jsx
+++ client/src/components/button/AlignButton.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from './AlignButton.module.css';
 
-export default function AlignButton() {
+/**
+ * @param {*} clickHandler1 최신순 눌렀을 때 동작할 이벤트
+ * @param {*} clickHandler2 좋아요순 눌렀을 때 동작할 이벤트
+ * @param {*} selected 선택된 버튼을 결정하는 state
+ */
+export default function AlignButton({ clickHandler1, clickHandler2, selected="최신순" }) {
   return (
-    <div>AlignButton</div>
-  )
+    <>
+      <button onClick={clickHandler1} className={selected === "최신순" ? styles.selected : styles.button}>
+        최신순
+      </button>
+
+      <button onClick={clickHandler2} className={selected === "좋아요순" ? styles.selected : styles.button}>
+        좋아요순
+      </button>
+    </>
+  );
 }
