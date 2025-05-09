6153d4ce9ba751315dadcded8d234614b4a46be3
ysc13245 <ysc13245@gmail.com>
2025-05-09 16:08:09 +0900

    feat: DropBoxButton Component

--- client/src/components/button/DropBoxButton.jsx
+++ client/src/components/button/DropBoxButton.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import styles from './DropBoxButton.module.css';
 
 export default function DropBoxButton() {
   return (
-    <div>DropBoxButton</div>
+    <div className={styles.wrapper}>
+      <div className={styles.btn} onClick={() => alert('수정')}>수정</div>
+      <div className={styles.btn} onClick={() => alert('삭제')}>삭제</div>
+    </div>
   )
 }
