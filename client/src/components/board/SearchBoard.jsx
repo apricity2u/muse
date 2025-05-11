85be9ec84e194d466facce2b6d51286a4e91ac67
apricity2u <gkdms9849@gmail.com>
2025-05-12 02:30:11 +0900

    chore: SearchBoard 컴포넌트 생성, 도서 검색 api 추가, css 설정

--- client/src/components/board/SearchBoard.jsx
+++ client/src/components/board/SearchBoard.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import SearchResultItem from '../item/SearchResultItem';
+import styles from './SearchBoard.module.css';
+import search from '../../assets/search.png';
+import reset from '../../assets/reset.png';
+import bookApi from '../../api/bookApi';
 
-export default function SearchBoard() {
+export default function SearchBoard({ clickHandler }) {
+  const [inputTitle, setInputTitle] = useState('');
+  const [bookList, setBookList] = useState([]);
+
+  const searchBooksHandler = async () => {
+    const response = await bookApi.searchTitle(inputTitle);
+    const data = response.data.data;
+
+    setBookList(data);
+  };
+
+  const inputHandler = (e) => {
+    setInputTitle(e.target.value);
+  };
+
+  const clickResetHandler = () => {
+    setInputTitle('');
+  };
+
   return (
-    <div>SearchBoard</div>
-  )
+    <div className={styles.wrapper}>
+      <div>
+        <div className={styles.searchBar}>
+          <img src={search} alt="search" className={styles.icon} />
+          <input type="text" placeholder="도서명 검색" value={inputTitle} onChange={inputHandler} />
+          <img src={reset} alt="reset" className={styles.icon} onClick={clickResetHandler} />
+        </div>
+        <hr className={styles.underLine} />
+      </div>
+      <ul>
+        {bookList.map((book) => {
+          const { id, title } = book;
+          <SearchResultItem id={id} title={title} clickHandler={clickHandler}></SearchResultItem>;
+        })}
+      </ul>
+    </div>
+  );
 }
