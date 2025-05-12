ffb83692896d6404dd95ac486784d2292b129548
apricity2u <gkdms9849@gmail.com>
2025-05-13 01:56:15 +0900

    feat: AlertModal Component

--- client/src/components/modal/AlertModal.jsx
+++ client/src/components/modal/AlertModal.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './AlertModal.module.css';
+import AlertButton from '../button/AlertButton';
 
-export default function AlertModal(content, ) {
+export default function AlertModal({ content, clickHandler1, clickHandler2 }) {
   return (
-    <div>AlertModal</div>
-  )
+    <div className={styles.modal}>
+      <div className={styles.content}>{'리뷰를 삭제하시겠습니까?'}</div>
+      <div>
+        <AlertButton color="gray" clickHandler={clickHandler1}>
+          취소
+        </AlertButton>
+        <AlertButton color="blue" clickHandler={clickHandler2}>
+          확인
+        </AlertButton>
+      </div>
+    </div>
+  );
 }
