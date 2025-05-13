dfd9178949297c49ff0ff8e38eee20967ad50bb8
apricity2u <gkdms9849@gmail.com>
2025-05-14 01:38:37 +0900

    refactor: TabButton 텍스트 children으로 전달

--- client/src/components/button/TabButton.jsx
+++ client/src/components/button/TabButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from './TabButton.module.css';
 
-export default function TabButton({ content }) {
-  const navigate = useNavigate();
-
-  return <div onClick={() => alert(content)} className={styles.btn}>{content}</div>;
+export default function TabButton({ children, clickHandler }) {
+  return (
+    <div onClick={clickHandler} className={styles.btn}>
+      {children}
+    </div>
+  );
 }
