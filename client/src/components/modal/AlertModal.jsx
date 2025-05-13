c7418628321711307cac21f9aa65f80fcb13a440
apricity2u <gkdms9849@gmail.com>
2025-05-13 12:40:47 +0900

    refactor: AlertModal 수정 및 주석 추가

--- client/src/components/modal/AlertModal.jsx
+++ client/src/components/modal/AlertModal.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import styles from './AlertModal.module.css';
 import AlertButton from '../button/AlertButton';
 
+/**
+  * @param content 모달창 내용
+  * @param clickHandler1 취소 버튼을 눌렀을 때 동작할 이벤트
+  * @param clickHandler2 확인 버튼을 눌렀을 때 동작할 이벤트 
+*/
 export default function AlertModal({ content, clickHandler1, clickHandler2 }) {
   return (
     <div className={styles.modal}>
-      <div className={styles.content}>{'리뷰를 삭제하시겠습니까?'}</div>
+      <div className={styles.content}>{content}</div>
       <div>
         <AlertButton color="gray" clickHandler={clickHandler1}>
           취소
