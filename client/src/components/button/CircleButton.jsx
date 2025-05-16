6d4bdd8af57e2ffbd9f86ed7062a00e72cca9f45
apricity2u <gkdms9849@gmail.com>
2025-05-16 12:46:01 +0900

    feat: common components (#13)
    * refactor: RoundButton 색상 및 이벤트 props 추가
    
    * refactor: 로고 디자인 변경 및 세부설정
    
    * refactor: TabButton 텍스트 children으로 전달
    
    * feat: authSlice 추가
    
    * feat: Header Components 기본 기능 구현, authApi 생성
    
    * chore: SubHeader component 삭제
    
    * feat: Header components 생성 및 스타일 설정
    
    * feat: 검색 모달 생성
    
    * refactor: CircleButton 내부 아이콘 경로 수정
    
    * feat: BookDetailItem component 생성
    
    * refactor: 로고 이미지 경로 수정
    
    * refactor: TabDropBoxButton click 이벤트 구체화 및 내부 아이콘 경로 변경

--- client/src/components/button/CircleButton.jsx
+++ client/src/components/button/CircleButton.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './CircleButton.module.css';
+import edit from '../../assets/edit.svg';
+import link from '../../assets/link.svg';
+import heart from '../../assets/heart.png';
 
-export default function CircleButton({ clickHandler, content = '좋아요' }) {
-  const icons = { '리뷰 작성': '/edit.svg', '링크 복사': '/link.svg', 좋아요: '/heart.svg' };
+export default function CircleButton({ clickHandler, children = '좋아요' }) {
+  const icons = { '리뷰 작성': edit, '링크 복사': link, 좋아요: heart };
 
   return (
     <div className={styles.wrapper}>
       <button className={styles.circle} onClick={clickHandler}>
-        <img src={icons[content]} alt={content} />
-        <div className={styles.tooltip}>{content}</div>
+        <img src={icons[children]} alt={children} />
+        <div className={styles.tooltip}>{children}</div>
       </button>
     </div>
   );
