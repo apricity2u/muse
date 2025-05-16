329f8d67a4242942a19efa9e84140dcee999413f
apricity2u <gkdms9849@gmail.com>
2025-05-16 12:18:49 +0900

    feat: BookDetailItem component 생성

--- client/src/components/item/BookDetailItem.jsx
+++ client/src/components/item/BookDetailItem.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styles from './BookDetailItem.module.css'
+import CircleButton from '../button/CircleButton';
 
 export default function BookDetailItem() {
   return (
-    <div>BookDetailItem</div>
-  )
+    <div className={styles.wrapper}>
+      <CircleButton>리뷰 작성</CircleButton>
+      <CircleButton>링크 복사</CircleButton>
+      <CircleButton>좋아요</CircleButton>
+    </div>
+  );
 }
