ac9d1373ccb6e6d14f7ce90c5d85172259d101f5
apricity2u <gkdms9849@gmail.com>
2025-05-17 01:55:47 +0900

    feat: 좋아요 api 생성 및 좋아요한 도서 상태 관리

--- client/src/api/bookApi.js
+++ client/src/api/bookApi.js
@@ -7,6 +7,16 @@ const bookApi = {
     const response = await api.get(`${ENDPOINT}`, { params: { title } });
     return response;
   },
+
+  postBookLikes: async (bookId) => {
+    const response = await api.post(`${ENDPOINT}/${bookId}/likes`);
+    return response;
+  },
+
+  deleteBookLikes: async (bookId) => {
+    const response = await api.delete(`${ENDPOINT}/${bookId}/likes`);
+    return response;
+  },
 };
 
 export default bookApi;
