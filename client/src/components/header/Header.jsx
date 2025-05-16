315d61700f17f55cf25529053ed4e38ad80c614c
apricity2u <gkdms9849@gmail.com>
2025-05-16 12:13:14 +0900

    feat: Header components 생성 및 스타일 설정

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,10 +3,12 @@ import styles from './Header.module.css';
 import Logo from '../Logo';
 import TabButton from '../button/TabButton';
 import RoundButton from '../button/RoundButton';
+import TabDropBoxButton from '../button/TabDropBoxButton';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import authApi from '../../api/authApi';
 import { logout } from '../../store/slices/authSlice';
+import authApi from '../../api/authApi';
+import SearchModal from '../modal/SearchModal';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -16,6 +18,7 @@ export default function Header() {
   const nickname = useSelector((state) => state.auth.nickname);
 
   const [isSearching, setIsSearching] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const writeHandler = () => {
     navigate('/reviews/create');
@@ -25,35 +28,62 @@ export default function Header() {
     setIsSearching(!isSearching);
   };
 
+  const openModalHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   const loginHandler = () => {
     navigate('/login');
   };
 
   const logoutHandler = async () => {
-    await authApi.logout();
-    dispatch(logout());
+    try {
+      await authApi.logout();
+      dispatch(logout());
+      navigate('/');
+    } catch (error) {
+      // TODO 추후 에러 처리 수정
+      console.error('로그아웃에 실패했습니다!');
+      console.log(error);
+    }
   };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <div className={styles.flexBox}>
-          <Logo></Logo>
-          <TabButton clickHandler={writeHandler}>글쓰기</TabButton>
-          <TabButton clickHandler={searchHandler}>검색</TabButton>
-        </div>
-        {!isLoggedIn ? (
-          <RoundButton clickHandler={loginHandler}>로그인</RoundButton>
-        ) : (
+    <>
+      {isSearching && (
+        <SearchModal
+          isSearching={isSearching}
+          setIsSearching={setIsSearching}
+          searchHandler={searchHandler}
+        ></SearchModal>
+      )}
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
           <div className={styles.flexBox}>
-            <div className={styles.nickname}>{nickname}님</div>
-            <RoundButton color="black" clickHandler={logoutHandler}>
-              로그아웃
-            </RoundButton>
+            <Logo></Logo>
+            <TabButton clickHandler={writeHandler}>글쓰기</TabButton>
+            <TabButton clickHandler={searchHandler}>검색</TabButton>
           </div>
-        )}
+          {!isLoggedIn ? (
+            <RoundButton clickHandler={loginHandler}>로그인</RoundButton>
+          ) : (
+            <div className={styles.flexBox}>
+              <div>
+                <div className={styles.nickname} onClick={openModalHandler}>
+                  {nickname}님
+                </div>
+                {isOpen && (
+                  <TabDropBoxButton isOpen={isOpen} setIsOpen={setIsOpen}></TabDropBoxButton>
+                )}
+              </div>
+              <RoundButton color="black" clickHandler={logoutHandler}>
+                로그아웃
+              </RoundButton>
+            </div>
+          )}
+        </div>
+        <hr className={styles.line} />
       </div>
-      <hr className={styles.line} />
-    </div>
+    </>
   );
 }
