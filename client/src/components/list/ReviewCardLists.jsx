76eec7c52829bf4f68ad4b291dac29cabd033318
apricity2u <gkdms9849@gmail.com>
2025-05-19 01:40:57 +0900

    feat: ReviewLists components 생성 및 스타일 설정

--- client/src/components/list/ReviewCardLists.jsx
+++ client/src/components/list/ReviewCardLists.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from './ReviewCardLists.module.css';
+import ReviewCard from '../card/ReviewCard';
 
-export default function ReviewCardLists() {
+export default function ReviewCardLists({ reviewCardLists  }) {
   return (
-    <div>ReviewCardLists</div>
-  )
+    <div className={styles.wrapper}>
+      {reviewCardLists?.map((reviewDetail) => {
+        const { review, book, user } = reviewDetail;
+        return <ReviewCard review={review} book={book} user={user}></ReviewCard>;
+      })}
+    </div>
+  );
 }
