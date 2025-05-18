d62443e7e3246bf4de7a43b5f94c56022d14598b
apricity2u <gkdms9849@gmail.com>
2025-05-18 18:56:32 +0900

    refactor: DropBoxButton Component 수정

--- client/src/components/button/DropBoxButton.jsx
+++ client/src/components/button/DropBoxButton.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 import styles from './DropBoxButton.module.css';
 
-export default function DropBoxButton() {
+export default function DropBoxButton({ clickHandler1, clickHandler2 }) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.btn} onClick={() => alert('수정')}>수정</div>
-      <div className={styles.btn} onClick={() => alert('삭제')}>삭제</div>
+      <div className={styles.btn} onClick={clickHandler1}>
+        수정
+      </div>
+      <div className={styles.btn} onClick={clickHandler2}>
+        삭제
+      </div>
     </div>
-  )
+  );
 }
