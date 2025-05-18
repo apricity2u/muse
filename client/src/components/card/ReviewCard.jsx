357af881728b2e1fc0b279ddecdb7e59c2949337
apricity2u <gkdms9849@gmail.com>
2025-05-19 00:20:43 +0900

    feat: ReviewCard component 뒷면 생성 및 스타일 설정

--- client/src/components/card/ReviewCard.jsx
+++ client/src/components/card/ReviewCard.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './ReviewCard.module.css';
-import dog from '../../assets/dog.jpg';
-import harry from '../../assets/harry.jpg';
+import leftIcon from '../../assets/left.png';
 import bookIcon from '../../assets/book.png';
 import disLikesIcon from '../../assets/heart.png';
 import menuIcon from '../../assets/menu-dots-vertical.png';
 import likesIcon from '../../assets/heart_filled.png';
 import reviewApi from '../../api/reviewApi';
+import bookApi from '../../api/bookApi';
 import { useSelector } from 'react-redux';
 import DropBoxButton from '../button/DropBoxButton';
 
@@ -17,10 +17,11 @@ export default function ReviewCard({ review, book, user }) {
   const { bookId, bookImageUrl, title, author, publisher, bookLikes, bookIsLike } = book;
   const { userId, nickname, userImageUrl } = user;
 
-  const [isOnMouse, setIsOnMouse] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [toggleCard, setToggleCard] = useState(false);
 
-  const [isLiked, setIsLiked] = useState(reviewIsLike);
+  const [likedReview, setLikedReview] = useState(reviewIsLike);
+  const [likedBook, setLikedBook] = useState(bookIsLike);
   const [reviewLikesCount, setReviewLikesCount] = useState(reviewLikes);
   const [bookLikesCount, setBookLikesCount] = useState(bookLikes);
 
@@ -28,8 +29,12 @@ export default function ReviewCard({ review, book, user }) {
   const modalRef = useRef(null);
   const memberId = useSelector((state) => state.auth.memberId);
 
-  const mouseOverHandler = () => {
-    setIsOnMouse(!isOnMouse);
+  const toggleCardHandler = () => {
+    setToggleCard(!toggleCard);
+  };
+
+  const clickTitleHandler = () => {
+    navigate(`/books/${bookId}`);
   };
 
   const clickProfileHandler = () => {
@@ -38,18 +43,30 @@ export default function ReviewCard({ review, book, user }) {
 
   const clickLikesHandler = async () => {
     try {
-      setIsLiked(!isLiked);
+      if (!toggleCard) {
+        setLikedReview(!likedReview);
 
-      if (isLiked) {
-        await reviewApi.postReviewLikes(reviewId);
-        setReviewLikesCount(reviewLikesCount + 1);
+        if (likedReview) {
+          await reviewApi.postReviewLikes(reviewId);
+          setReviewLikesCount(reviewLikesCount + 1);
+        } else {
+          await reviewApi.deleteReviewLikes(reviewId);
+          setReviewLikesCount(reviewLikesCount - 1);
+        }
       } else {
-        await reviewApi.deleteReviewLikes(reviewId);
-        setReviewLikesCount(reviewLikesCount - 1);
+        setLikedBook(!likedBook);
+
+        if (likedBook) {
+          await bookApi.postBookLikes(bookId);
+          setBookLikesCount(bookLikesCount + 1);
+        } else {
+          await bookApi.deleteBookLikes(bookId);
+          setBookLikesCount(bookLikesCount - 1);
+        }
       }
     } catch (error) {
       // TODO: 추후 에러 처리 보완
-      console.error('리뷰 좋아요 처리 실패');
+      console.error('좋아요 처리 실패');
     }
   };
 
@@ -88,21 +105,46 @@ export default function ReviewCard({ review, book, user }) {
   return (
     <div className={styles.card}>
       <div className={styles.topWrapper}>
-        <div className={styles.imageWrapper}>
-          <img src={reviewImageUrl} alt="cardImage" />
-          <div
-            className={styles.bookIconWrapper}
-            onMouseEnter={mouseOverHandler}
-            onMouseLeave={mouseOverHandler}
-          >
-            {!isOnMouse ? (
-              <img src={bookIcon} alt="bookIcon" className={styles.bookIcon} />
-            ) : (
-              <div className={styles.bookGrayText}>책 정보 보기</div>
-            )}
-          </div>
-        </div>
-        <div className={styles.content}>{content}</div>
+        {!toggleCard ? (
+          <>
+            <div className={styles.imageWrapper}>
+              <img src={reviewImageUrl} alt="cardImage" />
+              <div className={styles.bookIconWrapper}>
+                <img src={bookIcon} alt="bookIcon" className={styles.bookIcon} />
+                <div className={styles.bookGrayText} onClick={toggleCardHandler}>
+                  책 정보 보기
+                </div>
+              </div>
+            </div>
+            <div className={styles.content}>{content}</div>
+          </>
+        ) : (
+          <>
+            <div className={styles.imageWrapper}>
+              <div className={styles.bookImageWrapper}>
+                <img src={bookImageUrl} alt="cardImage" />
+              </div>
+              <div className={styles.leftIconWrapper} onClick={toggleCardHandler}>
+                <img src={leftIcon} alt="leftIcon" className={styles.leftIcon} />
+              </div>
+            </div>
+            <div className={styles.bookInfo}>
+              <h1 className={styles.title} onClick={clickTitleHandler}>
+                {title}
+              </h1>
+              <div className={styles.detailWrapper}>
+                <div className={styles.flexBox2}>
+                  <div>출판</div>
+                  <div>작가</div>
+                </div>
+                <div className={styles.flexBox2}>
+                  <div>{publisher}</div>
+                  <div>{author}</div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <hr />
       <div className={styles.bottomWrapper}>
@@ -112,36 +154,58 @@ export default function ReviewCard({ review, book, user }) {
           </div>
           <div className={styles.nickname}>{nickname}</div>
         </div>
-        <div className={styles.flexBox}>
-          {!isLiked ? (
-            <img
-              src={disLikesIcon}
-              alt="disLikesIcon"
-              className={styles.likeIcon}
-              onClick={clickLikesHandler}
-            />
-          ) : (
-            <img
-              src={likesIcon}
-              alt="likesIcon"
-              className={styles.likeIcon}
-              onClick={clickLikesHandler}
-            />
-          )}
-          <div className={styles.grayText}>좋아요</div>
-          <div className={styles.grayText}>{reviewLikesCount}</div>
-          {memberId == userId && (
-            <div className={styles.menuIconWrapper} ref={modalRef} onClick={clickMenuHandler}>
-              <img src={menuIcon} alt="menuIcon" className={styles.menuIcon} />
-              {isOpen && (
-                <DropBoxButton
-                  clickHandler1={editReviewHandler}
-                  clickHandler2={deleteReviewHandler}
-                ></DropBoxButton>
-              )}
-            </div>
-          )}
-        </div>
+        {!toggleCard ? (
+          <div className={styles.flexBox}>
+            {!likedReview ? (
+              <img
+                src={disLikesIcon}
+                alt="disLikesIcon"
+                className={styles.likeIcon}
+                onClick={clickLikesHandler}
+              />
+            ) : (
+              <img
+                src={likesIcon}
+                alt="likesIcon"
+                className={styles.likeIcon}
+                onClick={clickLikesHandler}
+              />
+            )}
+            <div className={styles.grayText}>좋아요</div>
+            <div className={styles.grayText}>{reviewLikesCount}</div>
+            {memberId == userId && (
+              <div className={styles.menuIconWrapper} ref={modalRef} onClick={clickMenuHandler}>
+                <img src={menuIcon} alt="menuIcon" className={styles.menuIcon} />
+                {isOpen && (
+                  <DropBoxButton
+                    clickHandler1={editReviewHandler}
+                    clickHandler2={deleteReviewHandler}
+                  ></DropBoxButton>
+                )}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className={styles.flexBox}>
+            {!likedBook ? (
+              <img
+                src={disLikesIcon}
+                alt="disLikesIcon"
+                className={styles.likeIcon}
+                onClick={clickLikesHandler}
+              />
+            ) : (
+              <img
+                src={likesIcon}
+                alt="likesIcon"
+                className={styles.likeIcon}
+                onClick={clickLikesHandler}
+              />
+            )}
+            <div className={styles.grayText}>좋아요</div>
+            <div className={styles.grayText}>{bookLikesCount}</div>
+          </div>
+        )}
       </div>
     </div>
   );
