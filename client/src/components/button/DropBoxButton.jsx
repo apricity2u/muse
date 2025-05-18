91a129671ea8b5aacd9be6ddb68713066c96a638
apricity2u <gkdms9849@gmail.com>
2025-05-19 02:49:44 +0900

    refactor: DropBoxButton Component 클릭 이벤트 추가

--- client/src/components/button/DropBoxButton.jsx
+++ client/src/components/button/DropBoxButton.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './DropBoxButton.module.css';
+import AlertModal from '../../components/modal/AlertModal';
 
-export default function DropBoxButton({ clickHandler1, clickHandler2 }) {
+export default function DropBoxButton({ clickHandler1, clickHandler2, openModal, setOpenModal }) {
+  const modalHandler = () => {
+    setOpenModal(true);
+  };
+
+  const closeModalHandler = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.btn} onClick={clickHandler1}>
-        수정
+    <>
+      {openModal && (
+        <AlertModal clickHandler1={closeModalHandler} clickHandler2={clickHandler2}>
+          리뷰를 삭제하시겠습니까?
+        </AlertModal>
+      )}
+      <div className={styles.wrapper}>
+        <div className={styles.btn} onClick={clickHandler1}>
+          수정
+        </div>
+        <div className={styles.btn} onClick={modalHandler}>
+          삭제
+        </div>
       </div>
-      <div className={styles.btn} onClick={clickHandler2}>
-        삭제
-      </div>
-    </div>
+    </>
   );
 }
