6980fe360d6aa7b822a24ccfd507f3bc5acb8a6f
apricity2u <gkdms9849@gmail.com>
2025-05-19 06:13:58 +0900

    chore: BookCardLists & ReviewCardLists Todo 추가

--- client/src/components/list/BookCardLists.jsx
+++ client/src/components/list/BookCardLists.jsx
@@ -3,10 +3,11 @@ import styles from './BookCardLists.module.css';
 import BookCard from '../card/BookCard';
 
 export default function BookCardLists({ bookCardLists }) {
+  // TODO: 도서가 없는 경우 처리
   return (
     <div className={styles.wrapper}>
       {bookCardLists?.map((bookDetail) => {
-        return <BookCard bookDetail={bookDetail}></BookCard>;
+        return <BookCard key={bookDetail.id} bookDetail={bookDetail}></BookCard>;
       })}
     </div>
   );
--- client/src/components/list/ReviewCardLists.jsx
+++ client/src/components/list/ReviewCardLists.jsx
@@ -3,11 +3,12 @@ import styles from './ReviewCardLists.module.css';
 import ReviewCard from '../card/ReviewCard';
 
 export default function ReviewCardLists({ reviewCardLists  }) {
+  // TODO: 리뷰가 없는 경우 예외처리
   return (
     <div className={styles.wrapper}>
       {reviewCardLists?.map((reviewDetail) => {
         const { review, book, user } = reviewDetail;
-        return <ReviewCard review={review} book={book} user={user}></ReviewCard>;
+        return <ReviewCard key={review.reviewId} review={review} book={book} user={user}></ReviewCard>;
       })}
     </div>
   );
