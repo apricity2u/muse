c4aeb0e490007389152553cf22e13c1406a89ecb
apricity2u <gkdms9849@gmail.com>
2025-05-19 16:34:44 +0900

    refactor: main에서 사용할 wrapper 클래스 추가

--- client/src/components/list/ReviewCardLists.jsx
+++ client/src/components/list/ReviewCardLists.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './ReviewCardLists.module.css';
 import ReviewCard from '../card/ReviewCard';
 
-export default function ReviewCardLists({ reviewCardLists  }) {
+export default function ReviewCardLists({ reviewCardLists, type = 'regular' }) {
   // TODO: 리뷰가 없는 경우 예외처리
   return (
-    <div className={styles.wrapper}>
+    <div className={styles[type]}>
       {reviewCardLists?.map((reviewDetail) => {
         const { review, book, user } = reviewDetail;
-        return <ReviewCard key={review.reviewId} review={review} book={book} user={user}></ReviewCard>;
+        return (
+          <ReviewCard key={review.reviewId} review={review} book={book} user={user}></ReviewCard>
+        );
       })}
     </div>
   );
