cf2571692af881a3a55c298b3451d51e0457c535
ysc13245 <ysc13245@gmail.com>
2025-05-24 05:01:56 +0900

    feat: 유저 프로필 조회 기능 구현

--- api/src/main/java/com/example/muse/domain/member/MemberController.java
+++ api/src/main/java/com/example/muse/domain/member/MemberController.java
@@ -1,7 +1,13 @@
 package com.example.muse.domain.member;
 
+import com.example.muse.domain.member.dto.GetProfileResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -9,10 +15,11 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
-//    @GetMapping("/profiles/{memberId}")
-//    public T updateProfile(@PathVariable Long memberId) {
-//        return memberService.getProfile();
-//    }
+    @GetMapping("/profiles/{memberId}")
+    public GetProfileResponseDto getProfile(@PathVariable UUID memberId) {
+
+        return memberService.getProfile(memberId);
+    }
 //
 //    @PatchMapping("/profiles/{memberId}")
 //    public T updateProfile(@PathVariable Long memberId) {
--- api/src/main/java/com/example/muse/domain/member/MemberService.java
+++ api/src/main/java/com/example/muse/domain/member/MemberService.java
@@ -1,11 +1,29 @@
 package com.example.muse.domain.member;
 
+import com.example.muse.domain.image.Image;
+import com.example.muse.domain.image.ImageRepository;
+import com.example.muse.domain.member.dto.GetProfileResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final ImageRepository imageRepository;
+
+    public GetProfileResponseDto getProfile(UUID memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        Image profileImage = imageRepository.findLastProfileImageByMemberId(memberId.toString())
+                .orElse(null);
+
+        return GetProfileResponseDto.from(member, profileImage);
+    }
 }
