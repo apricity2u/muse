84eaf11e8647f5d1afae4954bbb0dad53da2df53
ysc13245 <ysc13245@gmail.com>
2025-05-24 02:12:29 +0900

    feat: 프로필 이미지 조회 쿼리 작성

--- api/src/main/java/com/example/muse/domain/image/ImageRepository.java
+++ api/src/main/java/com/example/muse/domain/image/ImageRepository.java
@@ -1,6 +1,19 @@
 package com.example.muse.domain.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query(value = """
+            SELECT * FROM image
+                     WHERE member_id = :memberId
+                       AND image_type = 'PROFILE'
+                     ORDER BY created_at DESC
+                     LIMIT 1
+                                                                                       """, nativeQuery = true)
+    Optional<Image> findLastProfileImageNative(@Param("memberId") Long memberId);
 }
