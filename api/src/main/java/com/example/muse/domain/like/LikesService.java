76b5315372868978129dd16ce42f2f4048956e81
ysc13245 <ysc13245@gmail.com>
2025-05-24 09:54:19 +0900

    chore: LikesService 생성

---
+++ api/src/main/java/com/example/muse/domain/like/LikesService.java
@@ -0,0 +1,12 @@
+package com.example.muse.domain.like;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikesService {
+    private final LikesRepository likesRepository;
+}
