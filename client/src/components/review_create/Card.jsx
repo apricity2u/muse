1de1fa2625700d37e576e00920a90e2382a7f84a
apricity2u <gkdms9849@gmail.com>
2025-05-25 06:07:08 +0900

    feat: 리뷰 작성 시 미리보기 카드 생성

---
+++ client/src/components/review_create/Card.jsx
@@ -0,0 +1,29 @@
+import styles from './Card.module.css';
+import { useSelector } from 'react-redux';
+import auth from '../../store/slices/authSlice';
+
+export default function Card({ review }) {
+  const { imageUrl, content } = review;
+  const nickname = useSelector((state) => state.auth.nickname);
+  const userImageUrl = useSelector((state) => state.auth.imageUrl);
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.topWrapper}>
+        <div className={styles.imageWrapper}>
+          <img src={imageUrl} alt="reviewImage" />
+        </div>
+        <div className={styles.content}>{content}</div>
+      </div>
+      <hr />
+      <div className={styles.bottomWrapper}>
+        <div className={styles.flexBox}>
+          <div className={styles.profileImageWrapper}>
+            <img src={userImageUrl} alt="profileImage" />
+          </div>
+          <div className={styles.nickname}>{nickname}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
