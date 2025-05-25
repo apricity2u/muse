bed70c8a6b5f99b993ed86cbb32cd8d6c99e4f7b
apricity2u <gkdms9849@gmail.com>
2025-05-25 17:53:55 +0900

    fix: PaginationButton 이름 수정

--- client/src/components/common/button/PaginationButton.jsx
+++ client/src/components/common/button/PaginationButton.jsx
@@ -2,7 +2,7 @@ import styles from './PagiNationButton.module.css';
 import left from '../../../assets/icons/left.png';
 import right from '../../../assets/icons/right.png';
 
-export default function PageNationButton({ clickHandler, side = 'right' }) {
+export default function PaginationButton({ clickHandler, side = 'right' }) {
   const icons = { left: left, right: right };
 
   return (
