b2a52f01db039772ee55fb16cf810b76d2ac73e9
ysc13245 <ysc13245@gmail.com>
2025-05-30 06:49:11 +0900

    chore: 로그인 페이지 생성

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,22 @@
+import React from 'react';
+import styles from './Login.module.css';
+import kakao from '../assets/login-kakao.png';
+import naver from '../assets/login-naver.png';
+import google from '../assets/login-google.png';
+
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className={styles.wrapper}>
+      <img src="logo.png" alt="muse" style={{ width: '10%' }} />
+      <button className={styles.loginButton}>
+        <img src={kakao} alt="kakaoLogin" className={styles.loginButtonImage} />
+      </button>
+      <button className={styles.loginButton}>
+        <img src={naver} alt="naverLogin" className={styles.loginButtonImage} />
+      </button>
+      <button className={styles.loginButton}>
+        <img src={google} alt="googleLogin" className={styles.loginButtonImage} />
+      </button>
+    </div>
+  );
 }
