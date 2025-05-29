318107d0a88787f640c33c56ff71c1cc4fcbc83d
ysc13245 <ysc13245@gmail.com>
2025-05-29 21:09:51 +0900

    feat: 유저 dto 생성

--- api/src/main/java/com/example/muse/domain/review/dto/ReviewCardResponseDto.java
+++ api/src/main/java/com/example/muse/domain/review/dto/ReviewCardResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.muse.domain.review.dto;
 
 import com.example.muse.domain.book.dto.BookDto;
+import com.example.muse.domain.member.dto.MemberProfileDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +14,14 @@ import lombok.NoArgsConstructor;
 public class ReviewCardResponseDto {
     private BookDto book;
     private ReviewDto review;
+    private MemberProfileDto user;
 
-    public static ReviewCardResponseDto from(BookDto book, ReviewDto review) {
+    public static ReviewCardResponseDto from(BookDto book, ReviewDto review, MemberProfileDto user) {
 
         return ReviewCardResponseDto.builder()
                 .book(book)
                 .review(review)
+                .user(user)
                 .build();
     }
 }
