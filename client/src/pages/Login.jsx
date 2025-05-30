0e4d79e2d570b6a2729f5ab76985c00b319a8aed
ysc13245 <ysc13245@gmail.com>
2025-05-31 08:47:52 +0900

    fix: 이미지 교체

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,13 +3,14 @@ import styles from './Login.module.css';
 import kakao from '../assets/login-kakao.png';
 import naver from '../assets/login-naver.png';
 import google from '../assets/login-google.png';
+import authApi from '../api/authApi';
 
 export default function Login() {
   return (
     <div className={styles.wrapper}>
       <img src="logo.png" alt="muse" style={{ width: '10%' }} className={styles.logo} />
       <p className={styles.description}>책의 순간을 한 줄로 기록하는 공간</p>
-      <button className={styles.loginButton}>
+      <button className={styles.loginButton} onClick={authApi.print}>
         <img src={kakao} alt="kakaoLogin" className={styles.loginButtonImage} />
       </button>
       <button className={styles.loginButton}>
