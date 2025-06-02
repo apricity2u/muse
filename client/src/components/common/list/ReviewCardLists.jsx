9aa3cce6df84d2726199367d62707f5926a19551
apricity2u <gkdms9849@gmail.com>
2025-06-02 15:51:23 +0900

    refactor: refactor styles (#56)
    * refactor: ReviewCard 앞뒷면 컴포넌트 분리 및 스타일 재설정
    
    * refactor: 메인페이지 스타일 리팩토링
    
    * refactor: Header 반응형 디자인 구현
    
    * refactor: ReviewCardLists grid 형태로 변경
    
    * refactor: 메인페이지 스타일 임시저장

--- client/src/components/common/list/ReviewCardLists.jsx
+++ client/src/components/common/list/ReviewCardLists.jsx
@@ -1,14 +1,20 @@
 import styles from './ReviewCardLists.module.css';
 import ReviewCard from '../card/ReviewCard';
 
-export default function ReviewCardLists({ reviewCardLists, type = 'regular' }) {
+export default function ReviewCardLists({ reviewCardLists, type = 'regular', size = 'small' }) {
   // TODO: 리뷰가 없는 경우 예외처리
   return (
     <div className={styles[type]}>
       {reviewCardLists?.map((reviewDetail) => {
         const { review, book, user } = reviewDetail;
         return (
-          <ReviewCard key={review.reviewId} review={review} book={book} user={user}></ReviewCard>
+          <ReviewCard
+            key={review.reviewId}
+            review={review}
+            book={book}
+            user={user}
+            size={size}
+          ></ReviewCard>
         );
       })}
     </div>
