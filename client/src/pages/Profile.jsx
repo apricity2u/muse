001b2b390402186aafc6dc18d5f8667f33e6c016
ysc13245 <ysc13245@gmail.com>
2025-06-05 07:46:14 +0900

    feat: css 변경

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,12 +9,14 @@ export default function Profile() {
         src="https://recipe1.ezmember.co.kr/cache/recipe/2019/07/29/81147460a9faf7bdb78740b34758a5651.jpg"
         className={styles.profileImage}
       />
-      <RoundButton color="primary400">
+      <RoundButton color="primary700">
         <span style={{ color: `var(--primary-500)` }}>사진 수정</span>
       </RoundButton>
       <div className={styles.formField}>
-        <label htmlFor="nickname" className={styles.label}>닉네임</label>
-        <input type="text" className={styles.input} />
+        <label htmlFor="input" className={styles.label}>
+          닉네임
+        </label>
+        <input type="text" id="input" className={styles.input} />
         <div className={styles.warning}>
           <span>특수문자는 사용이 불가능합니다.</span>
           <span>0/20</span>
