88309d7bbe0245c3c18f258af7b74fb9112a62d6
ysc13245 <ysc13245@gmail.com>
2025-06-05 01:03:01 +0900

    feat: 개인정보 수정 페이지 퍼블리싱

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,26 @@
+import React from 'react';
+import RoundButton from '../components/common/button/RoundButton';
+import styles from './Profile.module.css';
+
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div className={styles.wrapper}>
+      <img
+        src="https://recipe1.ezmember.co.kr/cache/recipe/2019/07/29/81147460a9faf7bdb78740b34758a5651.jpg"
+        className={styles.profileImage}
+      />
+      <RoundButton color="primary400">
+        <span style={{ color: `var(--primary-500)` }}>사진 수정</span>
+      </RoundButton>
+      <div className={styles.formField}>
+        <label htmlFor="nickname" className={styles.label}>닉네임</label>
+        <input type="text" className={styles.input} />
+        <div className={styles.warning}>
+          <span>특수문자는 사용이 불가능합니다.</span>
+          <span>0/20</span>
+        </div>
+      </div>
+      <RoundButton>수정</RoundButton>
+    </div>
+  );
 }
