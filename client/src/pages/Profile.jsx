0abdee9463c9fc870843238809cbd5cc74bb8fa0
ysc13245 <ysc13245@gmail.com>
2025-06-05 19:48:05 +0900

    feat: 개인정보 수정 페이지

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,28 +1,110 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import RoundButton from '../components/common/button/RoundButton';
 import styles from './Profile.module.css';
+import profileApi from '../api/profileApi';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const MAX_LENGTH = 20;
+  const memberId = useSelector((state) => state.auth.memberId);
+
+  const [initialData, setInitialData] = useState({
+    nickname: '',
+    profileImageUrl: '',
+  });
+
+  const [formData, setFormData] = useState({
+    nickname: '',
+    imageFile: null,
+  });
+
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const imageFileRef = useRef(null);
+  const nicknameRef = useRef(null);
+  const [nicknameCount, setNicknameCount] = useState(0);
+
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const response = await profileApi.getProfile(memberId);
+        const { nickname, profileImageUrl } = response.data;
+
+        setInitialData({ nickname, profileImageUrl });
+        setFormData((prev) => ({ ...prev, nickname }));
+        setNicknameCount(nickname.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProfile();
+  }, []);
+
+  const handleNicknameChange = (e) => {
+    const nickname = e.target.value;
+    setFormData((prev) => ({ ...prev, nickname: nickname }));
+    setNicknameCount(nickname.length);
+  };
+  const onClickImageButton = () => {
+    if (imageFileRef.current) {
+      imageFileRef.current.click();
+    }
+  };
+
+  const onChangeImageFile = (e) => {
+    const imageFile = e.target.files[0];
+    if (!imageFile) return;
+
+    setFormData((prev) => ({ ...prev, imageFile }));
+    const objectUrl = URL.createObjectURL(imageFile);
+    setPreviewUrl(objectUrl);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const data = new FormData();
+      data.append('nickname', formData.nickname);
+      data.append('imageFile', formData.imageFile);
+      const response = await profileApi.updateProfile(memberId, formData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <img
-        src="https://recipe1.ezmember.co.kr/cache/recipe/2019/07/29/81147460a9faf7bdb78740b34758a5651.jpg"
-        className={styles.profileImage}
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageFileRef}
+        onChange={onChangeImageFile}
+        style={{ display: 'none' }}
+        id="input"
       />
-      <RoundButton color="primary700">
+      <img src={previewUrl || initialData.profileImageUrl} className={styles.profileImage} />
+      <RoundButton color="primary700" clickHandler={onClickImageButton}>
         <span style={{ color: `var(--primary-500)` }}>사진 수정</span>
       </RoundButton>
       <div className={styles.formField}>
-        <label htmlFor="input" className={styles.label}>
+        <label htmlFor="nicknameInput" className={styles.label}>
           닉네임
         </label>
-        <input type="text" id="input" className={styles.input} />
+        <input
+          type="text"
+          id="nicknameInput"
+          className={styles.input}
+          value={formData.nickname}
+          onChange={handleNicknameChange}
+          maxLength={MAX_LENGTH}
+          ref={nicknameRef}
+        />
         <div className={styles.warning}>
-          <span>특수문자는 사용이 불가능합니다.</span>
-          <span>0/20</span>
+          {/* <span>특수문자는 사용이 불가능합니다.</span> */}
+          <span>
+            {nicknameCount}/{MAX_LENGTH}
+          </span>
         </div>
       </div>
-      <RoundButton>수정</RoundButton>
+      <RoundButton clickHandler={handleSubmit}>수정</RoundButton>
     </div>
   );
 }
