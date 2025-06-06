91875af31444581dce54954c720948f83c1dd96f
apricity2u <gkdms9849@gmail.com>
2025-06-07 02:59:37 +0900

    feat: 마이페이지 및 좋아요 페이지 구현

--- client/src/pages/UserReviews.jsx
+++ client/src/pages/UserReviews.jsx
@@ -1,317 +1,82 @@
 import styles from './UserReviews.module.css';
 import ReviewCardLists from '../components/common/list/ReviewCardLists';
+import BookCardLists from '../components/common/list/BookCardLists';
 import SubTabButton from '../components/common/button/SubTabButton';
 import AlignButton from '../components/common/button/AlignButton';
-import dog from '../assets/dog.jpg';
-import harry from '../assets/harry.jpg';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import reviewApi from '../api/reviewApi';
+import bookApi from '../api/bookApi';
 
 export default function UserReviews() {
-  const reviewCardLists = [
-    {
-      review: {
-        reviewId: 1,
-        reviewImageUrl: dog,
-        content: '내용1',
-        reviewLikes: 12,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 1,
-        bookImageUrl: harry,
-        title: '해리포터1',
-        author: '작가1',
-        publisher: '출판사1',
-        bookLikes: 34,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 1,
-        nickname: '삼상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 2,
-        reviewImageUrl: dog,
-        content: '내용2',
-        reviewLikes: 56,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 2,
-        bookImageUrl: harry,
-        title: '해리포터2',
-        author: '작가1',
-        publisher: '출판사2',
-        bookLikes: 78,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 2,
-        nickname: '사상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3 해리포터3 해리포터3 해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 1,
-        reviewImageUrl: dog,
-        content: '내용1',
-        reviewLikes: 12,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 1,
-        bookImageUrl: harry,
-        title: '해리포터1',
-        author: '작가1',
-        publisher: '출판사1',
-        bookLikes: 34,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 1,
-        nickname: '삼상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 2,
-        reviewImageUrl: dog,
-        content: '내용2',
-        reviewLikes: 56,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 2,
-        bookImageUrl: harry,
-        title: '해리포터2',
-        author: '작가1',
-        publisher: '출판사2',
-        bookLikes: 78,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 2,
-        nickname: '사상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3 해리포터3 해리포터3 해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 1,
-        reviewImageUrl: dog,
-        content: '내용1',
-        reviewLikes: 12,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 1,
-        bookImageUrl: harry,
-        title: '해리포터1',
-        author: '작가1',
-        publisher: '출판사1',
-        bookLikes: 34,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 1,
-        nickname: '삼상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 2,
-        reviewImageUrl: dog,
-        content: '내용2',
-        reviewLikes: 56,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 2,
-        bookImageUrl: harry,
-        title: '해리포터2',
-        author: '작가1',
-        publisher: '출판사2',
-        bookLikes: 78,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 2,
-        nickname: '사상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-    {
-      review: {
-        reviewId: 3,
-        reviewImageUrl: dog,
-        content: '내용3',
-        reviewLikes: 90,
-        reviewIsLike: false,
-      },
-
-      book: {
-        bookId: 3,
-        bookImageUrl: harry,
-        title: '해리포터3 해리포터3 해리포터3 해리포터3',
-        author: '작가1',
-        publisher: '출판사3',
-        bookLikes: 11,
-        bookIsLike: false,
-      },
-
-      user: {
-        userId: 3,
-        nickname: '오상호',
-        userImageUrl: dog,
-      },
-    },
-  ];
-
   const user = useSelector((state) => state.auth);
-  const { imageUrl, nickname } = user;
+
+  const [reviewCardLists, setReviewCardLists] = useState([]);
+  const [bookCardLists, setBookCardLists] = useState([]);
+
+  const [isReview, setIsReview] = useState(true);
+  const [selected, setSelected] = useState('createdAt');
+  const [page, setPage] = useState({
+    totalPage: 0,
+    pageNo: 1,
+    hasPrevious: false,
+    hasNext: false,
+  });
+
+  const { memberId, imageUrl, nickname } = user;
+  const { totalPage, pageNo, hasPrevious, hasNext } = page;
+
+  const fetchUserReviewLists = async () => {
+    try {
+      const response = await reviewApi.getUserReviewLists(memberId, pageNo, selected);
+      const data = response.data;
+      const { totalPage, page, hasPrevious, hasNext, review } = data;
+
+      setReviewCardLists(review);
+      setPage((prev) => ({
+        ...prev,
+        totalPage: totalPage,
+        pageNo: page,
+        hasPrevious: hasPrevious,
+        hasNext: hasNext,
+      }));
+    } catch (error) {
+      alert('리뷰 목록을 불러오는데 실패했습니다.');
+      console.log(error);
+    }
+  };
+
+  const fetchUserBookLists = async () => {
+    try {
+      const response = await bookApi.getUserBookLists(memberId, pageNo, selected);
+      const data = response.data;
+      const { totalPage, page, hasPrevious, hasNext, book } = data;
+
+      setBookCardLists(book);
+      setPage((prev) => ({
+        ...prev,
+        totalPage: totalPage,
+        pageNo: page,
+        hasPrevious: hasPrevious,
+        hasNext: hasNext,
+      }));
+    } catch (error) {
+      alert('책 목록을 불러오는데 실패했습니다.');
+      console.log(error);
+    }
+  };
+
+  const sortListHandler = (sort) => {
+    if (sort !== selected) {
+      setSelected(sort);
+    }
+  };
+
+  useEffect(() => {
+    if (memberId) {
+      isReview ? fetchUserReviewLists() : fetchUserBookLists();
+    }
+  }, [isReview, selected]);
 
   return (
     <div className={styles.container}>
@@ -322,18 +87,30 @@ export default function UserReviews() {
           </div>
           <div className={styles.rightWrapper}>
             <div className={styles.nickname}>{nickname}</div>
-            <div className={styles.grayText}>작성한 리뷰 8건</div>
+            <div className={styles.grayText}>작성한 리뷰 {reviewCardLists?.length || 0}건</div>
           </div>
         </div>
         <div className={styles.subHeader}>
-          <SubTabButton content1="리뷰" content2="도서"></SubTabButton>
+          <SubTabButton content1="리뷰" content2="도서" setIsReview={setIsReview}></SubTabButton>
           <div className={styles.alignButton}>
-            <AlignButton></AlignButton>
+            <AlignButton
+              clickHandler1={() => sortListHandler('createdAt')}
+              clickHandler2={() => sortListHandler('likes')}
+              selected={selected === 'createdAt' ? '최신순' : '좋아요순'}
+            ></AlignButton>
           </div>
         </div>
-        <div className={styles.reviewWrapper}>
-          <ReviewCardLists reviewCardLists={reviewCardLists}></ReviewCardLists>
-        </div>
+        {!totalPage ? (
+          <div className={styles.noContentWrapper}>아직 작성한 리뷰가 없습니다.</div>
+        ) : (
+          <div className={styles.reviewWrapper}>
+            {isReview ? (
+              <ReviewCardLists reviewCardLists={reviewCardLists}></ReviewCardLists>
+            ) : (
+              <BookCardLists bookCardLists={bookCardLists}></BookCardLists>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
