6b720b1c12bea11248c1fe4c71c0ec30d0de2770
ysc13245 <ysc13245@gmail.com>
2025-06-07 12:15:18 +0900

    feat: 액세스 토큰 만료 예외처리

--- api/src/main/java/com/example/muse/global/security/jwt/JwtAuthenticationFilter.java
+++ api/src/main/java/com/example/muse/global/security/jwt/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.example.muse.global.security.jwt;
 
 import com.example.muse.domain.member.Member;
 import com.example.muse.domain.member.MemberRepository;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,6 +14,7 @@ import org.springframework.security.authentication.InsufficientAuthenticationExc
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -29,39 +31,38 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
-        String tokenString = jwtTokenUtil.getAccessTokenFromRequest(request);
+        try {
+            String tokenString = jwtTokenUtil.getAccessTokenFromRequest(request);
+            if (StringUtils.hasText(tokenString)) {
+                Jwt token = jwtTokenUtil.tokenFrom(tokenString);
+                if (!jwtTokenUtil.validateToken(token)) {
+                    throw new JwtException("유효하지 않은 JWT 토큰입니다.");
+                }
 
-        if (!StringUtils.hasText(tokenString)) {
+                UUID memberId = UUID.fromString(token.getSubject());
+                Member member = memberRepository.findById(memberId)
+                        .orElseThrow(() -> new InsufficientAuthenticationException("회원을 찾을 수 없습니다."));
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(new UsernamePasswordAuthenticationToken(member, null, List.of()));
+            }
 
             filterChain.doFilter(request, response);
-            return;
+
+        } catch (ExpiredJwtException |
+                 JwtValidationException e) {
+            response.setHeader("Token-Expired", "true");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 만료되었습니다.");
+        } catch (JwtException | IllegalArgumentException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다.");
         }
-
-        Jwt token = jwtTokenUtil.tokenFrom(tokenString);
-        if (!jwtTokenUtil.validateToken(token)) {
-
-            throw new JwtException("유효하지 않은 JWT 토큰입니다.");
-        }
-
-        UUID memberId;
-        try {
-            memberId = UUID.fromString(token.getSubject());
-        } catch (IllegalArgumentException e) {
-            throw new JwtException("JWT 서브젝트가 올바른 UUID 형식이 아닙니다.", e);
-        }
-
-        Member member = memberRepository.findById(memberId).orElseThrow(
-                () -> new InsufficientAuthenticationException("회원을 찾을 수 없습니다.")
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(
-                new UsernamePasswordAuthenticationToken(member, null, List.of())
-        );
-
-        filterChain.doFilter(request, response);
     }
+
 
 }
 
