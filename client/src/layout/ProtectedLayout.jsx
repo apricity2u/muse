42cb7d7083ccb9d0efb66ebe1308de145b4a1998
apricity2u <gkdms9849@gmail.com>
2025-06-10 02:53:39 +0900

    refactor: ProtectedLayout 조건문 변경

--- client/src/layout/ProtectedLayout.jsx
+++ client/src/layout/ProtectedLayout.jsx
@@ -3,12 +3,11 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedLayout({ children }) {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  return isLoggedIn ? (
-    children
-  ) : (
-    <>
-      {alert('로그인 후 이용 가능합니다.')}
-      <Navigate to="/login" replace />;
-    </>
-  );
+
+  if (!isLoggedIn) {
+    alert('로그인 후 이용 가능합니다.');
+    <Navigate to="/login" replace />
+  }
+
+  return children;
 }
