5c3c3ef45571593523533ea0c3feaacb70b34851
ysc13245 <ysc13245@gmail.com>
2025-06-26 08:39:11 +0900

    feat: 예외 처리

--- api/src/main/java/com/example/muse/global/common/exception/ErrorCode.java
+++ api/src/main/java/com/example/muse/global/common/exception/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     JWT_FAIL(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다.", 14),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", 20),
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다.", 21),
-    S3_FAIL(HttpStatus.BAD_REQUEST, "S3 처리에 실패하였습니다.", 22);
+    DUPLICATE(HttpStatus.BAD_REQUEST, "이미 존재하는 자원입니다.", 22),
+    S3_FAIL(HttpStatus.BAD_REQUEST, "S3 처리에 실패하였습니다.", 23);
 
 
     private final HttpStatus httpStatus;
