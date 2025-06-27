d40e66ae314a2708d72ead406be063367b6fa0f0
ysc13245 <ysc13245@gmail.com>
2025-06-28 02:54:23 +0900

    refactor: AppConstants 정적 필드 초기화 방식 개선
    @Value setter → @PostConstruct 패턴 적용

--- api/src/main/java/com/example/muse/global/common/config/AppConstants.java
+++ api/src/main/java/com/example/muse/global/common/config/AppConstants.java
@@ -1,14 +1,17 @@
 package com.example.muse.global.common.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppConstants {
+    @Value("${DOMAIN}")
+    private String domain;
     public static String IMAGE_PREFIX;
 
-    @Value("${DOMAIN}")
-    public void setDOMAIN(String DOMAIN) {
-        IMAGE_PREFIX = "https://image." + DOMAIN;
+    @PostConstruct
+    public void init() {
+        IMAGE_PREFIX = "https://image." + domain;
     }
 }
