e72f1011fe932ff849d2f873a51b8946a2ac67d6
ysc13245 <ysc13245@gmail.com>
2025-06-28 09:19:35 +0900

    fix: 캐시용 요청 메서드 GET으로 변경

--- api/src/main/java/com/example/muse/global/common/service/ImagePrefetchService.java
+++ api/src/main/java/com/example/muse/global/common/service/ImagePrefetchService.java
@@ -1,12 +1,14 @@
 package com.example.muse.global.common.service;
 
 import com.example.muse.domain.image.Image;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class ImagePrefetchService {
     private final RestTemplate restTemplate = new RestTemplateBuilder().build();
 
@@ -14,6 +16,7 @@ public class ImagePrefetchService {
     public void prefetchImageCache(Image image) {
 
         String imageUrl = image.getImageUrl();
-        restTemplate.headForHeaders(imageUrl);
+        log.info("prefetch image cache : {}", imageUrl);
+        restTemplate.getForEntity(imageUrl, String.class);
     }
 }
