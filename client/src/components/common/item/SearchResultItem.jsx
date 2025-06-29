9588176bca8b372d6232268b3447933492fc360a
apricity2u <gkdms9849@gmail.com>
2025-06-30 06:58:47 +0900

    feat: 검색시 검색어 강조하기

--- client/src/components/common/item/SearchResultItem.jsx
+++ client/src/components/common/item/SearchResultItem.jsx
@@ -1,9 +1,26 @@
 import styles from './SearchResultItem.module.css';
 
-export default function SearchResultItem({ id, title, clickHandler }) {
+export default function SearchResultItem({ id, title, keyword, clickHandler }) {
+  const highlightText = (text, keyword) => {
+    if (!keyword) return text;
+
+    const regex = new RegExp(`(${keyword})`, 'gi');
+    const parts = text.split(regex);
+
+    return parts.map((part, index) =>
+      part.toLowerCase() === keyword.toLowerCase() ? (
+        <span key={index} style={{ color: 'black', fontWeight: 'bold' }}>
+          {part}
+        </span>
+      ) : (
+        <span key={index}>{part}</span>
+      ),
+    );
+  };
+
   return (
     <li id={id} onClick={clickHandler} className={styles.searchResult}>
-      {title}
+      {highlightText(title, keyword)}
     </li>
   );
 }
