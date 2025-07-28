2e633fbd6afa5e4dca0b4ef685218c03be425e46
apricity2u <gkdms9849@gmail.com>
2025-07-29 00:29:05 +0900

    fix: protection router

--- client/src/layout/ProtectedLayout.jsx
+++ client/src/layout/ProtectedLayout.jsx
@@ -1,13 +1,19 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
-export default function ProtectedLayout({ children }) {
+export default function ProtectedLayout() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
+  useEffect(() => {
+    if (!isLoggedIn) {
+      alert('로그인 후 이용 가능합니다.');
+    }
+  }, [isLoggedIn]);
+
   if (!isLoggedIn) {
-    alert('로그인 후 이용 가능합니다.');
     return <Navigate to="/login" replace />;
   }
 
-  return children;
+  return <Outlet />;
 }
