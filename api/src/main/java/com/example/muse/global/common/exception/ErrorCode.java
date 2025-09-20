6ed3f7bbbdad201c1bd1ca97429f0c2c2bb01096
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-09-21 00:13:40 +0900

    Feature/monitoring (#108)
    * feat: 모니터링 docker compose yml 추가
    prometheus grafana exporter cadvisor
    
    * chore: prometheus actuaot 의존성 추가
    
    * chore: prometheus 설정
    
    * fix: 예외 처리 세분화
    
    * chore: 모니터링 컨테이너 설정
    
    * fix: prometheus 컨테이너 모니터 추가
    
    * feat: Prometheus 모니터링을 위한 nginx_status 경로 추가
    
    * docs: update .env.example
    
    * feat: grafana 프록시 설정

--- api/src/main/java/com/example/muse/global/common/exception/ErrorCode.java
+++ api/src/main/java/com/example/muse/global/common/exception/ErrorCode.java
@@ -8,14 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다.", 10),
-    LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "로그인에 실패하였습니다.", 11),
-    REISSUE_FAIL(HttpStatus.UNAUTHORIZED, "토큰 재발급에 실패하였습니다.", 12),
-    OAUTH_FAIL(HttpStatus.UNAUTHORIZED, "소셜 로그인에 실패하였습니다.", 13),
-    JWT_FAIL(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다.", 14),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", 20),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다.", 21),
-    DUPLICATE(HttpStatus.BAD_REQUEST, "이미 존재하는 자원입니다.", 22),
-    S3_FAIL(HttpStatus.BAD_REQUEST, "S3 처리에 실패하였습니다.", 23);
+    LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "로그인에 실패하였습니다.", 11),
+    REISSUE_FAIL(HttpStatus.UNAUTHORIZED, "토큰 재발급에 실패하였습니다.", 12),
+    OAUTH_FAIL(HttpStatus.UNAUTHORIZED, "소셜 로그인에 실패하였습니다.", 13),
+    JWT_FAIL(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다.", 14),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", 20),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다.", 21),
+    DUPLICATE(HttpStatus.BAD_REQUEST, "이미 존재하는 자원입니다.", 22),
+    S3_FAIL(HttpStatus.BAD_REQUEST, "S3 처리에 실패하였습니다.", 23),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 처리 오류가 발생했습니다.", 24);
 
 
     private final HttpStatus httpStatus;
