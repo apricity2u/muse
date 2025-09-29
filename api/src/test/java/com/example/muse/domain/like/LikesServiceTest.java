7a66efccb846123cd22a928e6fa14b6f3d96d8f3
ysc13245 <ysc13245@gmail.com>
2025-09-29 17:25:43 +0900

    test: 캐시 테스트 코드

--- api/src/test/java/com/example/muse/domain/like/LikesServiceTest.java
+++ api/src/test/java/com/example/muse/domain/like/LikesServiceTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 class LikesServiceTest {
     @Autowired
     private LikesService likesService;
